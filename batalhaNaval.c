77c058de76eff0f5effcbb4bb3d9e9964a40f98d
Elsin617 <elsonjunior831@gmail.com>
2025-04-22 23:17:48 +0000

    acrescentei a diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,29 +7,50 @@
 int main() {
     int tabuleiro[TAM][TAM];
 
-    // Inicializa todas as posições com 0 (água)
+    // Inicializa o tabuleiro com água
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
             tabuleiro[i][j] = AGUA;
         }
     }
 
-    // Posiciona o navio horizontal na linha 2 (índice 2), colunas 4, 5, 6
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[2][4 + i] = NAVIO;
-    }
+    // Posicionamento dos navios
+    tabuleiro[0][1] = NAVIO;
+    tabuleiro[0][9] = NAVIO;
 
-    // Posiciona o navio vertical na coluna 6 (índice 6), linhas 5, 6, 7
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[5 + i][6] = NAVIO; // sobreposição permitida
-    }
+    tabuleiro[1][1] = NAVIO;
+    tabuleiro[1][8] = NAVIO;
 
-    // Exibe o tabuleiro com letras A-J e linhas 1-10
-    printf("  TABULEIRO  BATALHA  NAVAL\n");
+    tabuleiro[2][2] = NAVIO;
+    tabuleiro[2][7] = NAVIO;
+
+    tabuleiro[3][3] = NAVIO;
+    tabuleiro[3][6] = NAVIO;
+
+    tabuleiro[4][4] = NAVIO;
+    tabuleiro[4][5] = NAVIO;
+
+    tabuleiro[5][4] = NAVIO;
+    tabuleiro[5][5] = NAVIO;
+
+    tabuleiro[6][3] = NAVIO;
+    tabuleiro[6][6] = NAVIO;
+
+    tabuleiro[7][1] = NAVIO;
+    tabuleiro[7][7] = NAVIO;
+
+    tabuleiro[8][1] = NAVIO;
+    tabuleiro[8][8] = NAVIO;
+
+    tabuleiro[9][0] = NAVIO;
+    tabuleiro[9][9] = NAVIO;
+
+    // Impressão do tabuleiro no formato da imagem
+    printf("  TABULEIRO BATALHA NAVAL\n");
     printf("  A B C D E F G H I J\n");
 
     for (int i = 0; i < TAM; i++) {
-        printf("%-2d", i + 1); // Exibe número da linha (1 a 10)
+        printf("%d ", i); // número da linha
         for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
@@ -39,4 +60,4 @@ int main() {
     return 0;
 }
 
-
+    
